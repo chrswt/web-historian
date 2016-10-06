d49b52f35f106afedc3e27e97158346915807689
chrswt <chriswt91@gmail.com>
2016-10-05 17:32:28 -0700

    Implement server get and post requests

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var qs = require('querystring');
 
 exports.headers = {
   'access-control-allow-origin': '*',
@@ -10,12 +11,35 @@ exports.headers = {
   'Content-Type': 'text/html'
 };
 
+var getStatic = function(res, site, callback) {
+  fs.readFile(path.join(__dirname, site), 'utf8', (err, data) => {
+    if (err) { 
+      res.writeHead(404, exports.headers);
+      res.end();
+    }
+    res.end(data, () => callback(res));
+  });
+};
+
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+
+  if (asset === '/') {
+    getStatic(res, '/public/index.html', callback);
+  } else {
+    getStatic(res, '../archives/sites' + asset, callback);
+  }
 };
 
+exports.postData = function(req, res) {
+  req.on('data', (data) => {
+    var site = qs.parse(Buffer(data).toString()).url;
+    var filePath = path.join(__dirname, '../archives/sites.txt');
+    fs.writeFile(filePath, site + '\n');
+  });
+};
 
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,25 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
 // require more modules/folders here!
 
+var actions = {
+  'GET': function(req, res) {
+    res.writeHead(200, helpers.headers);
+    helpers.serveAssets(res, req.url, (asset) => asset);  
+  },
+  'POST': function(req, res) {
+    res.writeHead(302, helpers.headers);
+    helpers.postData(req, res);
+    helpers.serveAssets(res, req.url, (asset) => asset);
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (!actions[req.method]) {
+    res.writeHead(404, helpers.headers);
+    res.end();
+  } else {
+    actions[req.method](req, res);
+  }
 };
