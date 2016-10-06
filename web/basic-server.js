8d941be02fe237d08f6c2acbf6b4927464270c71
chrswt <chriswt91@gmail.com>
2016-10-06 16:25:23 -0700

    Add cron with cron job for worker inside server

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,10 +1,25 @@
 var http = require('http');
 var handler = require('./request-handler');
 var initialize = require('./initialize.js');
+var Cron = require('cron').CronJob;
+var worker = require('../workers/htmlfetcher.js');
 
 // Why do you think we have this here?
 // HINT: It has to do with what's in .gitignore
+
 initialize('./archives');
+new Cron('0 * * * * *', function() {
+  console.log('new cron made');
+  worker();
+}, null, true, 'America/Los_Angeles');
+
+// try {
+//   new Cron('1 * * * * *', function() {
+//     console.log('this should not be printed');
+//   })
+// } catch (ex) {
+//   console.log('cron pattern not valid');
+// }
 
 var port = 8080;
 var ip = '127.0.0.1';
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,10 +1,13 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
 var archive = require('../helpers/archive-helpers');
-
 // does not support walmart.com (and only walmart.com), sorry fred
-archive.readListOfUrls((list) => { 
-  archive.nukeListOfUrls();
-  var list = list.slice(0, list.length - 1);
-  archive.downloadUrls(list);
-});
+
+module.exports = () => {
+  console.log('Set off worker in htmlfetcher!!!!!!!!');
+  archive.readListOfUrls((list) => { 
+    archive.nukeListOfUrls();
+    var list = list.slice(0, list.length - 1);
+    archive.downloadUrls(list);
+  });
+};
